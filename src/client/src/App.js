e45b1e6388bae8083dec839feebb0af0f0516a71
Geoff House <geoffhouse@users.noreply.github.com>
2023-01-02 22:58:09 +0000

    install mui

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,5 +1,13 @@
+import Page from "./components/Page";
+import CssBaseline from "@mui/material/CssBaseline";
+
 function App() {
-    return <>Chordal front end</>;
+    return (
+        <>
+            <CssBaseline />
+            <Page />
+        </>
+    );
 }
 
 export default App;
